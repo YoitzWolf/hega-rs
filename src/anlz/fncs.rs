fca4fca67991610e591f78125eb244ab4c9561a6
yoitz <yoitz@yandex.ru>
2025-04-30 19:18:52 +0300

    C to L test added

--- src/anlz/fncs.rs
+++ src/anlz/fncs.rs
@@ -7,6 +7,10 @@ def pseudorapidity(v1, v2):
 
 */
 
+use std::{fmt, fs::File, io::BufReader};
+
+use crate::fmt::{decoder::{DctCoding, EposDict}, oscar::{OSCEposHeader, OscarParticle}};
+
 use super::Particle;
 
 /// z-pseudorapidity
@@ -26,12 +30,77 @@ pub fn gamma<P: Particle>(p: &P, dec: &P::Decoder ) -> f64 {
 
 
 pub fn lab_momentum<P: Particle>(p: &P, dec: &P::Decoder ) -> (f64, f64, f64) {
-    let &mp = p.momentum(dec);
-    let pen = p.momentum_energy(dec);
-    let e = p.energy(dec);
-    let m = p.mass_energy(dec);
-    let gamma = e / m;
-    // let beta = pen / e;
-    let z = gamma * ( mp.2 + pen*gamma );
-    (mp.0, mp.1, z)
+    let &(x, y ,z) = p.momentum(dec);
+    // let pen = p.momentum_energy(dec);
+    // let e = p.energy(dec);
+    // let m = p.mass_energy(dec);
+    let gamma = gamma(p, dec);
+    let beta = beta(p, dec);
+    let z = gamma * (
+        z + beta*p.energy(dec) // pen*gamma
+    );
+    (x, y, z)
+}
+
+pub fn rapidity<P: Particle>(p: &P, dec: &P::Decoder ) -> f64 {
+    beta(p, dec).atanh()
+}
+
+
+#[test]
+fn test_lab_mom() {
+
+    let dict_lepto = EposDict::upload(
+        BufReader::new(File::open("./dicts/EPOS_LEPTONS.particles.txt").unwrap()),
+        DctCoding::EPOS,
+        None,
+    );
+    let dict_EPOS = EposDict::upload(
+        BufReader::new(File::open("./dicts/EPOS.particles.txt").unwrap()),
+        DctCoding::EPOS,
+        Some(dict_lepto.codes().cloned().collect())
+    );
+    drop(dict_lepto);
+
+    let pu0 = 199.239f64;
+
+    let p0 = (0.0f64, 0.0f64, -pu0);
+    let E0 = (p0.0.powi(2) + p0.1.powi(2) + p0.2.powi(2) + 172.592f64.powi(2)).sqrt();
+    let p = OscarParticle {
+        id: 0,
+        code: 1120,
+        state: 0,
+        p: p0,
+        p0: pu0,
+        mass: 172.592,
+        coords: (0. , 0. , 0.),
+        time: 0.,
+    };
+    let rap0 = rapidity(&p, &dict_EPOS);
+
+    // let t0 = p.energy(&dict_EPOS).powi(2) + p.
+
+    let np = lab_momentum(&p, &dict_EPOS);
+    println!("{:?}", np);
+    let T1 = (np.0.powi(2) + np.1.powi(2) + np.2.powi(2)).sqrt();
+    let p2 = OscarParticle {
+        id: 0,
+        code: 1120,
+        state: 0,
+        p: np,
+        p0: T1,
+        mass: 172.592,
+        coords: (0. , 0. , 0.),
+        time: 0.,
+    };
+
+    let E1 = p2.energy(&dict_EPOS);
+    let rap1 = rapidity(&p2, &dict_EPOS);
+
+    println!("E {}, {}", E0, E1);
+    println!("Rapidity {}, {}", rap0, rap1);
+    println!("Beta {}, {}",  beta(&p, &dict_EPOS), beta(&p2, &dict_EPOS));
+    println!("Gamma {}, {}", gamma(&p, &dict_EPOS), gamma(&p2, &dict_EPOS));
+    
+
 }
